733fb80a2a6a1249f341219c711fcef18ad14609
XainJan <ga771058@gmail.com>
2023-12-09 19:07:34 +0800

    first commit

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
+import './assets/js/index.js';
+import './assets/js/admin.js';
+import './assets/js/scroll.js';
 console.log("Hello world!");
